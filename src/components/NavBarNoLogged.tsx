420488c9ddbf76780887b5194681fd66bfabba99
parvezhossainme <108608854+parvezhossainme@users.noreply.github.com>
2025-08-11 18:15:12 +0600

    initial project push with authenication

---
+++ src/components/NavBarNoLogged.tsx
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import React from "react";
+
+// Notification Icon add
+import { BellIcon } from "@heroicons/react/24/outline";
+import { SignedOut, SignInButton, SignUpButton } from "@clerk/nextjs";
+
+const NavBarNoLogged = () => {
+    return (
+        <header className="flex justify-between p-4 bg-amber-100">
+            <div className="text-2xl font-bold">
+              <Link href="/">
+                UNICANTEEN LOGO
+              </Link>
+            </div>
+            <div>
+                {/* Search Bar */}
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    className="w-96 border border-red-300 rounded-3xl px-3 py-1 focus:outline-none focus:border-red-500"
+                />
+            </div>
+            <nav className="flex items-center">
+                <BellIcon className="text-red-700 mr-4 border rounded-full h-10 w-10 p-1 " />
+                <ul className="flex gap-4">
+                    <li>
+                        <SignedOut>
+                            <SignInButton mode="modal">
+                                <button className="signBtn">Sign In</button>
+                            </SignInButton>
+                        </SignedOut>
+                    </li>
+                    <li>
+                        <SignedOut>
+                            <SignUpButton mode="modal">
+                                <button className="signBtn">Sign Up</button>
+                            </SignUpButton>
+                        </SignedOut>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default NavBarNoLogged;
