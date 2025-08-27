d3e4ae58411eeb74f1db2e90ff13dd91d30a98ad
parvezhossainme <108608854+parvezhossainme@users.noreply.github.com>
2025-08-27 23:50:40 +0600

    redirecting checking

--- src/app/(home)/(auth)/sign-in/page.tsx
+++ src/app/(home)/(auth)/sign-in/page.tsx
@@ -1,10 +1,11 @@
+"use client"
 import { useRouter } from "next/navigation";
 import React from "react";
 
 const SignIn = () => {
     const router = useRouter();
     router.push("/");
-    return <div>SignIn</div>;
+    return <div>Redirecting to Home Page @parvezhossainme </div>;
 };
 
 export default SignIn;
