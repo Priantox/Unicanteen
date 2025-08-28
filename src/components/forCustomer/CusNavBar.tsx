97f6fbafe6e328c3e7433324ca38c8150a476efb
parvezhossainme <108608854+parvezhossainme@users.noreply.github.com>
2025-08-28 20:37:55 +0600

    2-customer shops food fetch added

--- src/components/forCustomer/CusNavBar.tsx
+++ src/components/forCustomer/CusNavBar.tsx
@@ -19,18 +19,6 @@ export default function CusNavBar() {
                         </Link>
                     </div>
 
-                    {/* Search Bar */}
-                    <div className="flex-1 max-w-xl mx-8">
-                        <div className="relative">
-                            <input
-                                type="text"
-                                placeholder="Search for food..."
-                                className="w-full bg-gray-100 dark:bg-gray-700 px-4 py-2 pr-8 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                            <Search className="absolute right-3 top-2.5 h-5 w-5 text-gray-400" />
-                        </div>
-                    </div>
-
                     {/* Right Side Items */}
                     <div className="flex items-center space-x-4">
                         {/* Cart */}
@@ -50,7 +38,7 @@ export default function CusNavBar() {
                         </button>
 
                         {/* User Profile */}
-                        <UserButton afterSignOutUrl="/sign-in" />
+                        <UserButton afterSignOutUrl="/" />
                     </div>
                 </div>
             </div>
